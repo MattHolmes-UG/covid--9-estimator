234f060f2eff8efcbaae4d162ec8c466d8fa7d4c
MattHolmes <matthewochonogorug@gmail.com>
2020-04-11 23:46:14 +0100

    test 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable radix */
 const estimatePowerFactor = (data) => {
   const { periodType, timeToElapse } = data;
@@ -40,18 +41,8 @@ const estimateImpact = (data, typeOfImpact) => {
   const hospitalBedsByRequestedTime = parseInt(availableBeds - severeCasesByRequestedTime);
 
   // challenge 3
-  let casesForICUByRequestedTime = parseInt(infectionsByRequestedTime * 0.05);
-  let casesForVentilatorsByRequestedTime = parseInt(infectionsByRequestedTime * 0.02);
-  if (periodType === 'weeks') {
-    casesForICUByRequestedTime = parseInt(casesForICUByRequestedTime / 7);
-    casesForVentilatorsByRequestedTime = parseInt(casesForVentilatorsByRequestedTime / 7);
-    // console.log('for weeks', casesForICUByRequestedTime, casesForVentilatorsByRequestedTime);
-  }
-  if (periodType === 'months') {
-    casesForICUByRequestedTime = parseInt(casesForICUByRequestedTime / 30);
-    casesForVentilatorsByRequestedTime = parseInt(casesForVentilatorsByRequestedTime / 30);
-    // console.log('for months', casesForICUByRequestedTime, casesForVentilatorsByRequestedTime);
-  }
+  const casesForICUByRequestedTime = parseInt(infectionsByRequestedTime * 0.05);
+  const casesForVentilatorsByRequestedTime = parseInt(infectionsByRequestedTime * 0.02);
   // gradr seems to be working with * 7 and 30
   const dollarsInFlight = estimateDailyEconomicImpact(data, infectionsByRequestedTime);
   return {
@@ -68,6 +59,20 @@ const estimateImpact = (data, typeOfImpact) => {
 const covid19ImpactEstimator = (data) => {
   const impact = estimateImpact(data, 'normal');
   const severeImpact = estimateImpact(data, 'severe');
+
+  if (data.periodType === 'weeks') {
+    impact.casesForICUByRequestedTime = parseInt(impact.casesForICUByRequestedTime / 7);
+    impact.casesForVentilatorsByRequestedTime = parseInt(impact.casesForVentilatorsByRequestedTime / 7);
+    severeImpact.casesForICUByRequestedTime = parseInt(severeImpact.casesForICUByRequestedTime / 7);
+    severeImpact.casesForVentilatorsByRequestedTime = parseInt(severeImpact.casesForVentilatorsByRequestedTime / 7);
+    // console.log('for weeks', casesForICUByRequestedTime, casesForVentilatorsByRequestedTime);
+  }
+  if (data.periodType === 'months') {
+    impact.casesForICUByRequestedTime = parseInt(impact.casesForICUByRequestedTime / 30);
+    impact.casesForVentilatorsByRequestedTime = parseInt(impact.casesForVentilatorsByRequestedTime / 30);
+    severeImpact.casesForICUByRequestedTime = parseInt(severeImpact.casesForICUByRequestedTime / 30);
+    severeImpact.casesForVentilatorsByRequestedTime = parseInt(severeImpact.casesForVentilatorsByRequestedTime / 30);
+  }
 
   return {
     data,
